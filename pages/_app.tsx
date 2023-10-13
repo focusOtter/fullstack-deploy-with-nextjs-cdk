d007cfb7e883b2092b38b51d9957f37921a5b0dd
focusotter <mtliendo@focusotter.com>
2023-10-13 15:40:28 -0500

    final test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Amplify } from 'aws-amplify'
+import cdkOutputs from '../outputs.json'
+
+Amplify.configure({
+	aws_appsync_graphqlEndpoint: cdkOutputs.BackendStack.AppSyncAPIURL,
+	aws_appsync_region: cdkOutputs.BackendStack.awsAppsyncRegion,
+	aws_appsync_authenticationType: cdkOutputs.BackendStack.AppSyncAuthType,
+	aws_appsync_apiKey: cdkOutputs.BackendStack.AppSyncAPIKey,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
